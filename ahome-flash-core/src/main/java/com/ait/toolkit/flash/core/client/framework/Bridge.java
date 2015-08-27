5433ce405a9b97ca6920c79ca54f3a90d08c88c3
eemi210 <ekambi@ahome-it-com>
2015-08-28 00:25:15 +0200

    Injecting resources on bridgename creation

--- ahome-flash-core/src/main/java/com/ait/toolkit/flash/core/client/framework/Bridge.java
+++ ahome-flash-core/src/main/java/com/ait/toolkit/flash/core/client/framework/Bridge.java
@@ -1,17 +1,17 @@
 /*
- Copyright (c) 2014 Ahomé Innovation Technologies. All rights reserved.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ * Copyright (c) 2014 Ahomé Innovation Technologies. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.ait.toolkit.flash.core.client.framework;
@@ -26,110 +26,109 @@ import com.google.gwt.core.client.JsArrayString;
  */
 public class Bridge extends JavaScriptObject {
 
-	private static String bridgeName;
+    private static String bridgeName;
 
-	protected Bridge() {
-	}
+    protected Bridge() {}
 
-	public static void setBridgeName(String name) {
-		bridgeName = name;
-	}
+    public static void setBridgeName( String name ) {
+        bridgeName = name;
+    }
 
-	/**
-	 * Gets the bridge instance
-	 */
-	public static native Bridge get()/*-{
-		var bridgeName = @com.ait.toolkit.flash.core.client.framework.Bridge::bridgeName;
-		//$wnd.console.log(bridgeName);
-		return $wnd.FABridge[bridgeName] ? $wnd.FABridge[bridgeName]
-				: $wnd.FABridge["Flash4j"];
-	}-*/;
+    /**
+     * Gets the bridge instance
+     */
+    public static native Bridge get()/*-{
+                                     var bridgeName = @com.ait.toolkit.flash.core.client.framework.Bridge::bridgeName;
+                                     //$wnd.console.log(bridgeName);
+                                     return $wnd.FABridge[bridgeName] ? $wnd.FABridge[bridgeName]
+                                     : $wnd.FABridge["Flash4j"];
+                                     }-*/;
 
-	public static native Bridge get(String bridgeName)/*-{
-		@com.ait.toolkit.flash.core.client.framework.Bridge::bridgeName = bridgeName;
-		return $wnd.FABridge[bridgeName];
-	}-*/;
+    public static native Bridge get( String bridgeName )/*-{
+                                                        @com.ait.toolkit.flash.core.client.framework.Bridge::bridgeName = bridgeName;
+                                                        return $wnd.FABridge[bridgeName];
+                                                        }-*/;
 
-	private final native JavaScriptObject create(String id) /*-{
-		//preventing from calling the constructor of DisplayObjectContainer
-		//since it's an Abstract class
-		if (id != "flash.display.DisplayObjectContainer") {
-			try {
-				var obj = this.create(id);
-				return obj;
-			} catch (e) {
-				$wnd.console.log(e.message);
-			}
-		}
-		return null;
-	}-*/;
+    private final native JavaScriptObject create( String id ) /*-{
+                                                              //preventing from calling the constructor of DisplayObjectContainer
+                                                              //since it's an Abstract class
+                                                              if (id != "flash.display.DisplayObjectContainer") {
+                                                              try {
+                                                              var obj = this.create(id);
+                                                              return obj;
+                                                              } catch (e) {
+                                                              $wnd.console.log(e.message);
+                                                              }
+                                                              }
+                                                              return null;
+                                                              }-*/;
 
-	public final native void addEventHandler(String id, String type, EventHandler handler)/*-{
-		this
-				.addEventListener(
-						id,
-						type,
-						function(event) {
-							var eventObject = @com.ait.toolkit.flash.core.client.events.Event::new(Lcom/google/gwt/core/client/JavaScriptObject;)(event);
-							handler.@com.ait.toolkit.flash.core.client.events.handlers.EventHandler::onEvent(Lcom/ait/toolkit/flash/core/client/events/Event;)(eventObject);
-						});
-	}-*/;
+    public final native void addEventHandler( String id, String type, EventHandler handler )/*-{
+                                                                                            this
+                                                                                            .addEventListener(
+                                                                                            id,
+                                                                                            type,
+                                                                                            function(event) {
+                                                                                            var eventObject = @com.ait.toolkit.flash.core.client.events.Event::new(Lcom/google/gwt/core/client/JavaScriptObject;)(event);
+                                                                                            handler.@com.ait.toolkit.flash.core.client.events.handlers.EventHandler::onEvent(Lcom/ait/toolkit/flash/core/client/events/Event;)(eventObject);
+                                                                                            });
+                                                                                            }-*/;
 
-	private final native JavaScriptObject create(String id, JsArrayString args) /*-{
-		return this.create(id, args);
-	}-*/;
+    private final native JavaScriptObject create( String id, JsArrayString args ) /*-{
+                                                                                  return this.create(id, args);
+                                                                                  }-*/;
 
-	private final native JavaScriptObject create(String id, JsArray<JavaScriptObject> args) /*-{
-		return this.create(id, args);
-	}-*/;
+    private final native JavaScriptObject create( String id, JsArray<JavaScriptObject> args ) /*-{
+                                                                                              return this.create(id, args);
+                                                                                              }-*/;
 
-	/**
-	 * gets the class reference of the object with the given id
-	 * 
-	 * @param id
-	 *            the id
-	 * @return the java script object
-	 */
-	public final native JavaScriptObject classRef(String id) /*-{
-		return this.classRef(id);
-	}-*/;
+    /**
+     * gets the class reference of the object with the given id
+     * 
+     * @param id
+     *            the id
+     * @return the java script object
+     */
+    public final native JavaScriptObject classRef( String id ) /*-{
+                                                               return this.classRef(id);
+                                                               }-*/;
 
-	/**
-	 * Gets the root of the flash application
-	 * 
-	 * @return the root
-	 */
-	public final native JavaScriptObject getRoot() /*-{
-		return this.root();
-	}-*/;
+    /**
+     * Gets the root of the flash application
+     * 
+     * @return the root
+     */
+    public final native JavaScriptObject getRoot() /*-{
+                                                   return this.root();
+                                                   }-*/;
 
-	/**
-	 * Creates a flash object with the given id
-	 * 
-	 * @param <T>
-	 *            the generic type
-	 * @param id
-	 *            the id
-	 * @return the t
-	 */
-	public static <T extends JavaScriptObject> T createObject(String id) {
-		return get().create(id).cast();
-	}
+    /**
+     * Creates a flash object with the given id
+     * 
+     * @param <T>
+     *            the generic type
+     * @param id
+     *            the id
+     * @return the t
+     */
+    public static <T extends JavaScriptObject> T createObject( String id ) {
+        return get().create( id ).cast();
+    }
 
-	/**
-	 * Debug.
-	 * 
-	 * @param id
-	 *            the id
-	 * @return the string
-	 */
-	public final native String debug(String id)/*-{
-		$wnd.console.log('creating :  ' + id);
-		var o = this.create(id);
-		$wnd.console.log(o);
-		for ( var v in o) {
-			$wnd.console.log('- : ' + v);
-		}
-	}-*/;
+    /**
+     * Debug.
+     * 
+     * @param id
+     *            the id
+     * @return the string
+     */
+    public final native String debug( String id )/*-{
+                                                 $wnd.console.log('creating :  ' + id);
+                                                 var o = this.create(id);
+                                                 $wnd.console.log(o);
+                                                 for ( var v in o) {
+                                                 $wnd.console.log('- : ' + v);
+                                                 }
+                                                 }-*/;
 
 }
--- ahome-flash-core/src/main/java/com/ait/toolkit/flash/core/client/framework/Core.java
+++ ahome-flash-core/src/main/java/com/ait/toolkit/flash/core/client/framework/Core.java
@@ -1,32 +1,42 @@
 /*
- Copyright (c) 2014 Ahomé Innovation Technologies. All rights reserved.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ * Copyright (c) 2014 Ahomé Innovation Technologies. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.ait.toolkit.flash.core.client.framework;
 
 import java.util.Random;
 
+import com.ait.toolkit.core.client.Util;
+import com.ait.toolkit.flash.core.client.resources.FlashCoreResources;
+import com.ait.toolkit.flash.core.detect.resources.FlashDetectResources;
+
 public class Core {
     public static String BRIDGE_NAME = generateBridgeName();
+
+    static {
+        Util.injectJs( FlashDetectResources.INSTANCE.js() );
+        Util.injectJs( FlashCoreResources.INSTANCE.faBridge() );
+        Util.injectJs( FlashCoreResources.INSTANCE.swfObject() );
+    }
 
     /**
      * Generate and random bridge name
      */
     private static String generateBridgeName() {
-        if (BRIDGE_NAME == null) {
+        if( BRIDGE_NAME == null ) {
             Random random = new Random();
-            BRIDGE_NAME = "Flash4j-" + Long.toString(Math.abs(random.nextLong()), 36);
+            BRIDGE_NAME = "Flash4j-" + Long.toString( Math.abs( random.nextLong() ), 36 );
         }
         return BRIDGE_NAME;
     }
