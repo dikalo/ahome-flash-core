5ff1f1b3544241e6622ca3bd7dca479a18c3b71d
eemi210 <ekambi@ahome-it-com>
2015-09-01 13:28:50 +0200

    adds couple of new pdf classes

---
+++ ahome-flash-core/src/main/java/com/ait/toolkit/flash/purepdf/client/pdf/PdfAction.java
@@ -0,0 +1,54 @@
+package com.ait.toolkit.flash.purepdf.client.pdf;
+
+import com.ait.toolkit.core.client.JsObject;
+import com.ait.toolkit.flash.purepdf.client.elements.Paragraph;
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class PdfAction extends JsObject {
+
+    public PdfAction( JavaScriptObject obj ) {
+        jsObj = obj;
+    }
+
+    public final native void addAuthor( String value )/*-{
+		var peer = this.@com.ait.toolkit.core.client.JsObject::getJsObj()();
+		peer.addAuthor(value);
+    }-*/;
+
+    public final native void addTitle( String value )/*-{
+		var peer = this.@com.ait.toolkit.core.client.JsObject::getJsObj()();
+		peer.addTitle(value);
+    }-*/;
+
+    public final native void addCreator( String value )/*-{
+		var peer = this.@com.ait.toolkit.core.client.JsObject::getJsObj()();
+		peer.addCreator(value);
+    }-*/;
+
+    public final native void addSubject( String value )/*-{
+		var peer = this.@com.ait.toolkit.core.client.JsObject::getJsObj()();
+		peer.addSubject(value);
+    }-*/;
+
+    public final native void addKeywords( String value )/*-{
+		var peer = this.@com.ait.toolkit.core.client.JsObject::getJsObj()();
+		peer.addKeywords(value);
+    }-*/;
+
+    public final native void setViewerPreferences( int value )/*-{
+		var peer = this.@com.ait.toolkit.core.client.JsObject::getJsObj()();
+		peer.setViewerPreferences(value);
+    }-*/;
+
+    public final native void open()/*-{
+		var peer = this.@com.ait.toolkit.core.client.JsObject::getJsObj()();
+		peer.open();
+    }-*/;
+
+    public final native void close()/*-{
+		var peer = this.@com.ait.toolkit.core.client.JsObject::getJsObj()();
+		peer.close();
+    }-*/;
+
+
+}
